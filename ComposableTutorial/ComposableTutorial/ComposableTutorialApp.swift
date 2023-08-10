2958c0728306a7203b11835ea5e5ed9037f759fe
dudu <whqtkf12@naver.com>
2023-08-10 13:46:07 +0900

    TCA Tutorial Project 시작

---
+++ ComposableTutorial/ComposableTutorial/ComposableTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableTutorialApp.swift
+//  ComposableTutorial
+//
+//  Created by LS-MAC-00213 on 2023/08/10.
+//
+
+import SwiftUI
+
+@main
+struct ComposableTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
